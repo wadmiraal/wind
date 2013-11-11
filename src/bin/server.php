2a65bd75f320ce2af25a7ea8c0898143b645a323
wadmiraal <wad@wadmiraal.net>
2013-11-11 11:50:41 +0000

    Add stub code for the server script.

--- src/bin/server.php
+++ src/bin/server.php
@@ -4,4 +4,16 @@
  * @file
  */
 
-require 'vendor/autoload.php';
+// Fetch the Composer autoload logic.
+require '../../../../vendor/autoload.php';
+
+try {
+    // todo
+    $logger = new Monolog();
+} catch (Exception $e) {
+    die("This example script requires Monolog. Aborting.");
+}
+
+$router = new Wind\Server\Router();
+$wind = new Wind\Server\EndPoint($logger, $router);
+$wind->run();
